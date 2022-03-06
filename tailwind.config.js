1dedfeb1be6112a5291e00efcdea098b0754f310
Holden <sumiliholden@gmail.com>
2022-03-06 16:48:25 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./assets/**/*.{css}",
+    "./components/*.{vue,js}",
+    "./components/**/*.{vue,js}",
+    "./pages/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./*.{vue,js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+  ],
+};
